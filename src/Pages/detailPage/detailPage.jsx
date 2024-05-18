9822e87d18bbebaffe5289f0f642b1065628db1c
Azi-jsProger <aziorozbayev2008@gmail.com>
2024-05-19 00:47:30 +0600

    game prestol

--- src/Pages/detailPage/detailPage.jsx
+++ src/Pages/detailPage/detailPage.jsx
@@ -5,70 +5,92 @@ import './style.css'
 import IsLoading from "../../components/loading/isLoading";
 
 const DetailPage = () => {
+    const {id} = useParams();
 
-  const {id} =  useParams()
-
-    console.log(id)
-
-    const [isLoading, setIsLoading] = useState(false)
-    const [character, setCharacter] = useState({})
-    const [error , setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false);
+    const [character, setCharacter] = useState({});
+    const [error , setError] = useState(null);
 
     const getCharacter = async () => {
-        setIsLoading(true)
+        setIsLoading(true);
         try {
-            const response = await axiosInstance.get(`api/v2/Characters/${id}`)
-            setCharacter(response.data)
-        }catch (e) {
+            const response = await axiosInstance.get(`api/v2/Characters/${id}`);
+            setCharacter(response.data);
+        } catch (e) {
             if (e?.response?.status === 400) {
-                setError('вы запросе ошибка!')
+                setError('вы запросе ошибка!');
             } else if (e?.response?.status === 404) {
-                setError('сервер не найден!')
+                setError('сервер не найден!');
             } else if (e?.response?.status === 500) {
-                setError('внутренняя ошибка сервера!')
+                setError('внутренняя ошибка сервера!');
             } else {
-                setError('интернет отключен!')
+                setError('интернет отключен!');
             }
-
         } finally {
-            setIsLoading(true)
+            setIsLoading(false);
         }
-
-    }
+    };
 
     useEffect(() => {
-        getCharacter()
+        getCharacter();
     }, []);
 
     return (
-        <div className='character'>
+        <div className='container'>
             {isLoading ?
-                <IsLoading />
+                <div className='loading'>
+                    <IsLoading />
+                </div>
                 :
-                <div className='container'>
+                <div className='info-character'>
 
-                    <h2>{character.fullName}</h2>
+                    <h1>Character Detail</h1>
 
-                    <div className='big-photo'>
-                        <img src={character.imageUrl} alt=""/>
-                    </div>
+                    <div className='detail-info'>
 
-                    <div>
-                        {character.family}
-                    </div>
+                        <div className='name-img'>
+                            <h2>{character.fullName}</h2>
+
+                            <div className='big-photo'>
+                                <img className='photo' src={character.imageUrl} alt=""/>
+                            </div>
+                        </div>
+
+                        <div className='details'>
+
+                            <div className="left">
+                                <h2>ID:</h2>
+                                <h2>First Name:</h2>
+                                <h2>Last Name:</h2>
+                                <h2>Full Name:</h2>
+                                <h2>Title:</h2>
+                                <h2>Family:</h2>
+                                <h2>Image:</h2>
+                                <h2>Image URL:</h2>
+                            </div>
+
+                            <div className="right">
+                                <h2>{character.id}</h2>
+                                <h2> {character.firstName}</h2>
+                                <h2> {character.lastName}</h2>
+                                <h2> {character.fullName}</h2>
+                                <h2> {character.title}</h2>
+                                <h2> {character.family}</h2>
+                                <h2> {character.image}</h2>
+                                <h2> {character.imageUrl}</h2>
+                            </div>
+
+                        </div>
 
 
-                    <div className='div-error'>
-                        {error && <h2 className='error'>{error}</h2>}
+                        <div className='div-error'>
+                            {error && <h2 className='error'>{error}</h2>}
+                        </div>
                     </div>
 
                 </div>
-
-
             }
         </div>
-
-
     );
 };
 
--- src/Pages/mainPage/mainPage.jsx
+++ src/Pages/mainPage/mainPage.jsx
@@ -1,71 +1,75 @@
-import React, {useEffect, useState} from 'react';
-import {axiosInstance} from "../../utils/API/API";
-import './style.css'
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { axiosInstance } from "../../utils/API/API";
+import './main.css';
+import { useNavigate } from "react-router-dom";
+import IsLoading from "../../components/loading/isLoading";
 
 const MainPage = () => {
-
-    const [isLoading, setIsLoading] = useState(false)
-    const [characters, setCharacters] = useState([])
-    const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(false);
+    const [characters, setCharacters] = useState([]);
+    const navigate = useNavigate();
 
     const getCharacter = async () => {
-        setIsLoading(true)
+        setIsLoading(true);
         try {
-            const response = await axiosInstance.get('api/v2/Characters')
-            setCharacters(response.data)
-        }catch (e) {
+            const response = await axiosInstance.get('api/v2/Characters');
+            setCharacters(response.data);
+        } catch (e) {
             if (e.response.status === 400) {
-                console.log('Aziret write error')
+                console.log('Aziret write error');
             } else if (e.response.status === 404) {
-                console.log('lirvbnfm')
+                console.log('lirvbnfm');
             } else if (e.response.status === 500) {
-                console.log('fuygsruhvruoi')
-            }
-            else {
-                console.log('Not information')
+                console.log('fuygsruhvruoi');
+            } else {
+                console.log('Not information');
             }
         } finally {
-            setIsLoading(false)
+            setIsLoading(false);
         }
-
-    }
-
-    console.log(characters)
+    };
 
     useEffect(() => {
-        getCharacter()
+        getCharacter();
     }, []);
 
     return (
-        <div>
+        <div className='container-info'>
             {isLoading ?
-                <h1 className='loading'>Loading</h1>
-                :
-                <div className='character-list-wrap'>
-                    {characters.map((item,idx,arrey) => {
-                            return (
-                                <div
-                                    key={idx}
-                                    className='character-list'
-                                    onClick={() => navigate(`/${item.id}`)}
-                                >
-
-                                    <div>
-                                        {item.id}
-                                    </div>
-
-                                    <div>
-                                        {item.fullName}
-                                    </div>
-
-                                    <div className='photo'>
-                                        <img className='photoImg' src={item.imageUrl} alt=""/>
-                                    </div>
-                                </div>
-                            )
-                    })}
+                <div className='loading'>
+                    <IsLoading/>
                 </div>
+                :
+
+                <div className='character'>
+
+                    <h1 className='list'>Character List</h1>
+
+                    <div className='character-list-wrap'>
+
+                        <div className="hard-info">
+                            <h1>Id</h1>
+                            <h1>Name</h1>
+                            <h1>Image</h1>
+                        </div>
+
+                        {characters.map((item, idx) => (
+                            <div
+                                key={idx}
+                                className='character-list'
+                                onClick={() => navigate(`/${item.id}`)}
+                            >
+                                <div>{item.id}</div>
+                                <div>{item.fullName}</div>
+
+                                <div className='photo'>
+                                    <img src={item.imageUrl} alt={item.fullName}/>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             }
         </div>
     );
